1c6c94775397a217c223e920ecf81465ee1266e8
Mitali Agrawal <mitaliagrawal4321@gmail.com>
2022-03-13 23:39:23 +0530

    Added layout files for user side of the app

---
+++ app/src/main/java/com/example/bus_e_no/PersonalDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.bus_e_no;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_details);
+
+
+    }
+}
---
+++ bus-e-no/src/main/java/com/example/bus_e_no/MyActivity.java
@@ -0,0 +1,17 @@
+package com.example.bus_e_no;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
