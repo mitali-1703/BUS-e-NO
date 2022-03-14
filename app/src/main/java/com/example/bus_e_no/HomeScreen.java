53feebd5ba74cdfbcea66908da8c5a2bba5927fd
Mitali Agrawal <mitaliagrawal4321@gmail.com>
2022-03-14 23:30:48 +0530

    Added navigation drawer

---
+++ app/src/main/java/com/example/bus_e_no/HomeScreen.java
@@ -0,0 +1,60 @@
+package com.example.bus_e_no;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeScreen extends AppCompatActivity {
+
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView = findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
+    }
+
+    // override the onOptionsItemSelected()
+    // function to implement
+    // the item click listener callback
+    // to open and close the navigation
+    // drawer when the icon is clicked
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
--- app/src/main/java/com/example/bus_e_no/PersonalDetailsActivity.java
+++ app/src/main/java/com/example/bus_e_no/PersonalDetailsActivity.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.bus_e_no.databinding.ActivityPersonalDetailsBinding;
+
 public class PersonalDetailsActivity extends AppCompatActivity {
+
+    ActivityPersonalDetailsBinding b;
+    String[] courses = {"B.Com.","B.Sc.Agriculture","B.Sc.Biotech","B.Sc.Hospitality & Hotel Mgmt.","B.Sc.Microbiology",
+                        "B.Sc.PCM","B.of Hotel Mgmt.","B.A. J&MC","B.A. Other","B.Tech. Civil","B.Tech. CSE","B.Tech. ECE",
+                        "B.Tech. EE","B.Tech. ME","BBA","BCA","Design Courses","Law","M.Sc. Agriculture","M.Sc. Biotech","M.Sc. CS",
+                        "M.Sc. Microbiology","M.Sc. PCM","M.A. J&MC","M.Tech. Civil","M.Tech. CSE","M.Tech. ECE","M.Tech. EE",
+                        "M.Tech. ME","MBA","MCA","PG Diploma"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_details);
+        b=ActivityPersonalDetailsBinding.inflate(getLayoutInflater());
+        setContentView(b.getRoot());
 
-
+//        b.autoCompleteCourse
     }
 }
